5b7757fa17fb8a1f01e95dc3d692e48763e7a4f9
HiddenMotives <HiddenMotives@users.noreply.github.com>
2018-08-24 18:22:17 -0600

    Create Card.java

---
+++ blackjack/Card.java
@@ -0,0 +1,70 @@
+package blackjack;
+
+/**
+ * @author HiddenMotives
+ *
+ * Card class helps facilitate in creating a Deck of playing cards, by managing
+ * face and suit of a card in separate variables.
+ *
+ * Released under the GNU General Public License v3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+public class Card {
+
+    private final String Face;
+    private final String Suit;
+
+    /**
+     *
+     * @param Face - Either String value A, J, Q, K or a String value integer 2
+     * - 10 (inclusive)
+     * @param Suit - String value H, D, S or C
+     */
+    public Card(String Face, String Suit) {
+        this.Face = Face;
+        this.Suit = Suit;
+    }
+
+    /**
+     *
+     * @return String - Face value of the card
+     */
+    public String getFace() {
+        return this.Face;
+    }
+
+    /**
+     *
+     * @return String - Suit value of the card
+     */
+    public String getSuit() {
+        return this.Suit;
+    }
+
+    /**
+     * Get the numerical face value of the card
+     *
+     * @return Integer - Face value of the card
+     */
+    public Integer getValue() {
+        switch (this.getFace()) {
+            case "A":
+                return 1;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            default:
+                return Integer.parseInt(this.getFace());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getFace() + this.getSuit();
+    }
+
+}
